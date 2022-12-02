2c61be0f05bcfa1d1f3215266cf33ccf6257f246
Mostakimjaba <110723107+Mostakimjaba@users.noreply.github.com>
2022-12-02 23:12:14 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,77 @@
+
+//function textChange() {
+	//document.getElementById('p').innerHTML="welcome";
+	// body...
+//}
+$(document).ready(function(){
+	$('button').click(function(){
+		alert("hello world");
+
+
+	});
+	$( "#draggable" ).draggable();
+	$( "#accordion" ).accordion();
+	 var availableTags = [
+      "ActionScript",
+      "AppleScript",
+      "Asp",
+      "BASIC",
+      "C",
+      "C++",
+      "Clojure",
+      "COBOL",
+      "ColdFusion",
+      "Erlang",
+      "Fortran",
+      "Groovy",
+      "Haskell",
+      "Java",
+      "JavaScript",
+      "Lisp",
+      "Perl",
+      "PHP",
+      "Python",
+      "Ruby",
+      "Scala",
+      "Scheme"
+    ];
+    $( "#tags" ).autocomplete({
+      source: availableTags
+    });
+     $( "#draggable1" ).draggable();
+    $( "#droppable" ).droppable({
+      drop: function( event, ui ) {
+        $( this )
+          .addClass( "ui-state-highlight" )
+          .find( "p" )
+            .html( "Dropped!" );
+      }
+    });
+    $( "#datepicker" ).datepicker();
+     $( "#slider" ).slider();
+
+
+
+
+});
+
+$( function() {
+    $( "#slider" ).slider();
+     var state = true;
+    $( "#button1" ).on( "click", function() {
+      if ( state ) {
+        $( "#effect" ).animate({
+          backgroundColor: "#aa0000",
+          color: "#fff",
+          width: 500
+        }, 1000 );
+      } else {
+        $( "#effect" ).animate({
+          backgroundColor: "#fff",
+          color: "#000",
+          width: 240
+        }, 1000 );
+      }
+      state = !state;
+    });
+  } );
